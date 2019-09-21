567e96131991f6a0541239c0c4aa131c08538115
Ellie <j.chen.ellie@gmail.com>
2019-09-21 05:12:31 -0400

    Outline done, need to add functionality

--- sbu-hack-proj/src/App.js
+++ sbu-hack-proj/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import img from './uranus.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    fileUploaded: false
+  };
+
+  render() {
+    return (
+      <div id="entity">
+        <h1>Is it Uranus?</h1>
+        <div id="display">
+          <div id="display-img">
+            {/* Want to make img displayed change based on whether fileUploaded true or not */}
+            <img src={img} />
+          </div>
+        </div>
+        <div id="form">
+          <br />
+          <input className="text-input" type="text" placeholder="Type link here..." /><br />
+          <input className="button" type="button" value="UPLOAD LINK" />
+          {/* <input className="button" type="button" value="UPLOAD FILE" /> */}
+        </div>
+        <br />
+
+        {/* Want to make this message change based on API response */}
+        <h1>That's definitely uranus *wink*</h1>
+
+      </div>
+
+    )
+  }
 }
 
 export default App;
